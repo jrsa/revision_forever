2c02fde9a811769fc7788634b179d42e57d666d5
James Anderson <jrsa@jrsa.co>
2017-12-01 14:26:51 -0800

    add defines to switch openGL version

--- src/main.cpp
+++ src/main.cpp
@@ -1,13 +1,14 @@
 #include "ofMain.h"
 #include "ofApp.h"
 
+#define GL_MAJOR 2
+#define GL_MINOR 1
+
 //========================================================================
 int main( ){
-	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
 
-	// this kicks off the running of my app
-	// can be OF_WINDOW or OF_FULLSCREEN
-	// pass in width and height too:
-	ofRunApp(new ofApp());
-
+    ofGLWindowSettings settings;
+    settings.setGLVersion(GL_MAJOR, GL_MINOR);
+    ofCreateWindow(settings);
+    ofRunApp(new ofApp());
 }
